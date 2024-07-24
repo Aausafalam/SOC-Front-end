0e37619e3b54bba612f3d307eda836410d5d47a4
Aausafalam <aausaf@c3ihub.iitk.ac.in>
2024-07-24 16:56:33 +0530

    [added layout]

---
+++ src/layout/Navbar/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from "./index.module.css"
+
+const NavBar = () => {
+  return (<div className={styles.container}>
+       <img src="./assets/companyLogo.png" alt="" />
+       <nav>
+        <ul>
+            {/* <li></li> */}
+        </ul>
+       </nav>
+  </div>
+  )
+}
+
+export default NavBar
---
+++ src/pages/AssetsManagementModule/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./index.module.css"
+
+const AssetsManagementModule = () => {
+  return (<div className={styles.container}>
+     <h1>Assets Management Module</h1>
+  </div>
+  )
+}
+
+export default AssetsManagementModule
