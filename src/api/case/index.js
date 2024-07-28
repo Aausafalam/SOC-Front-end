8e34f69be0b6bbb6a9e01bf4dbc63b9d150d5a68
anuj <anujarya6121998@gmail.com>
2024-07-28 14:23:28 +0530

    test

--- src/api/case/index.js
+++ src/api/case/index.js
@@ -2,7 +2,7 @@ import { constants } from "../../utils/constants";
 import apiClient from "../config";
 
 export const getCaseList = async (signal) => {
-    const response = await apiClient.get("/caseData2.json", { signal });
+    const response = await apiClient.get(constants.API_URLS.CASE, { signal });
     return response.data.data;
 };
 
